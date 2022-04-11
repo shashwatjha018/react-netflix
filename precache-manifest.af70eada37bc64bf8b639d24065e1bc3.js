77d5a75e257246305ee380ce3a63c0195b447740
shashwatjha018 <shashwat.jha2019@vitstudent.ac.in>
2022-04-12 01:18:34 +0530

    Updates

---
+++ precache-manifest.af70eada37bc64bf8b639d24065e1bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39f0d8bd58ab36ac5feb3bfed97b6b5",
+    "url": "/react-netflix/index.html"
+  },
+  {
+    "revision": "a424b924f2cc6ca5ed6d",
+    "url": "/react-netflix/static/css/main.b548183b.chunk.css"
+  },
+  {
+    "revision": "f737cf7124382cada4e4",
+    "url": "/react-netflix/static/js/2.6d534e33.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/react-netflix/static/js/2.6d534e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a424b924f2cc6ca5ed6d",
+    "url": "/react-netflix/static/js/main.32830df4.chunk.js"
+  },
+  {
+    "revision": "6072596f829a9ae3bbc5",
+    "url": "/react-netflix/static/js/runtime-main.ffb8085b.js"
+  }
+]);
